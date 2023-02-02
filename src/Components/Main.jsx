e7119f8fe962d21216d3cff3b3f5055f77b19178
MUH. RESTU ALIZA AKBAR <muhrestualizaakbar@gmail.com>
2023-02-03 02:15:25 +0800

    First Commit

---
+++ src/Components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="flex h-screen bg-[#03001C]">
+      <div className="m-auto text-center text-white">
+        <h1 className="mainName text-5xl py-6">Hi, I'm MUH RESTU ALIZA AKBAR</h1>
+        <small className="text-3xl">I'm a front-end web devloper</small>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/Navbar/Humberger/Burger.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { bool, func } from "prop-types";
+import { Styleburger } from "./Burgerstyle";
+
+const Burger = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Burger;
